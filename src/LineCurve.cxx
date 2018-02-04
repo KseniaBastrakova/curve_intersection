0f2cc9cd52be3282a25d07894cdc327cdf051545
KseniaBastrakova <bastrakova.kseinia@gmail.com>
2018-02-04 21:49:26 +0300

    Add line

---
+++ src/LineCurve.cxx
@@ -0,0 +1,41 @@
+#include "LineCurve.hxx"
+
+LineCurve::LineCurve (const Point& thePoint1, const Point& thePoint2) {
+
+	myDirectingVector = Point(thePoint2.X - thePoint1.X, thePoint2.Y - thePoint1.Y);
+	myStartPoint = thePoint1;
+	myEndPoint = thePoint2;
+
+}
+
+Point LineCurve::GetPoint (double t) const {
+	Point aPointValue;
+	aPointValue.X = myDirectingVector.X * t + myStartPoint.X;
+	aPointValue.Y = myDirectingVector.Y * t + myStartPoint.Y;
+	return aPointValue;
+}
+
+Range LineCurve::GetRange() const {
+	return Range (0.0, 1.0);
+}
+
+Point LineCurve::GetDerivedPoint (double t) const {
+	return myDirectingVector;
+}
+
+Point LineCurve::GetStartPoint() const {
+	return myStartPoint;
+}
+
+Point LineCurve::GetEndPoint() const {
+	return myEndPoint;
+}
+
+std::string LineCurve::GetName() const {
+	return "Line";
+}
+
+bool LineCurve::EqualTo(const ICurve& theOther) const {
+	const LineCurve& aOther = static_cast<const LineCurve&> (theOther);
+	return (aOther.GetStartPoint() == this->GetStartPoint() && aOther.GetEndPoint() == this->GetEndPoint());
+}
---
+++ src/LineCurve.hxx
@@ -0,0 +1,22 @@
+#pragma once
+#include "Base_Structures.hxx"
+#include "ICurve.hxx"
+
+
+class LineCurve : public ICurve {
+public:
+	LineCurve (const Point& thePoint1, const Point& thePoint2);
+	virtual Range GetRange() const;
+	virtual Point GetPoint (double t) const;
+	Point GetDerivedPoint (double t) const;
+	Point GetStartPoint() const;
+	Point GetEndPoint() const;
+
+	virtual std::string GetName() const;
+	bool EqualTo (const ICurve& theOther) const;
+private:
+	Point myDirectingVector;
+	Point myStartPoint;
+	Point myEndPoint;
+	Range myRange;
+};
