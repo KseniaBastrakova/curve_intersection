6a0ee49e368934df6b2bb484bfedc584276e9f0f
KseniaBastrakova <bastrakova.kseinia@gmail.com>
2018-02-04 22:41:20 +0300

    Add evaluation lipschitz constant

---
+++ src/EvaluationLipschitzConstant.hxx
@@ -0,0 +1,17 @@
+#pragma once
+#include "Base_Structures.hxx"
+#include <functional>
+
+class LipschitzConstantEvaluator {
+public:
+	LipschitzConstantEvaluator (std::function<double(double,double)> theFunction, Range theRangeX, Range theRangeY):
+		myFunction (theFunction), myRangeX (theRangeX), myRangeY (theRangeY) {}
+
+	double Evaluate (int theResolution) const;
+
+private:
+	double myLipschitzConstant;
+	std::function<double(double, double)> myFunction;
+	Range myRangeX;
+	Range myRangeY;
+};
