4f7034b4a1198fa3a3a06a05105a51b89227e5ec
KseniaBastrakova <bastrakova.kseinia@gmail.com>
2018-02-04 22:35:58 +0300

    Add Circle serializer

---
+++ src/CircleCurveSerializer.hxx
@@ -0,0 +1,17 @@
+#pragma once
+#include "CircleCurve.hxx"
+#include "FormattedCurveSerializer.hxx"
+#include <ostream>
+
+class CircleCurveSerializer :public FormattedCurveSerializer {
+	
+public:
+	CircleCurveSerializer (Format theFormat) :FormattedCurveSerializer (theFormat) {}
+	virtual std::unique_ptr<ICurve> Read (std::istream& theInput);
+	virtual void Write (std::ostream& theOutput, const ICurve& theCurve);
+
+	virtual std::string GetHeaderName();
+
+	virtual ~CircleCurveSerializer() = default;
+};
+
