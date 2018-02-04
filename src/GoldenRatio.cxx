ca0ae41f326f427ef0809496406aa4d6e105cd0d
KseniaBastrakova <bastrakova.kseinia@gmail.com>
2018-02-04 22:40:04 +0300

    Add golden ratio method

---
+++ src/GoldenRatio.cxx
@@ -0,0 +1,26 @@
+#include "GoldenRatio.hxx"
+
+double GoldenRatio::Run() {
+
+	double aGRConstant = 1.6180339887;
+	double aBegin = myRange.Begin;
+	double aEnd = myRange.End;
+	double X1 = aEnd - (aEnd - aBegin) / aGRConstant;
+	double X2 = aBegin + (aEnd - aBegin) / aGRConstant;
+	double Y1 = myFunction(X1);
+	double Y2 = myFunction(X2);
+	while ((aEnd - aBegin) > myEps) {
+		X1 = aEnd - (aEnd - aBegin) / aGRConstant;
+		X2 = aBegin + (aEnd - aBegin) / aGRConstant;
+		Y1 = myFunction(X1);
+	    Y2 = myFunction(X2);
+		if (Y1 > Y2) {
+			aBegin = X1;
+		} else {
+			aEnd = X2;
+		}
+
+	}
+	return (aEnd + aBegin) / 2;
+
+}
---
+++ src/GoldenRatio.hxx
@@ -0,0 +1,15 @@
+#pragma once
+#include "Base_Structures.hxx"
+#include <functional>
+
+class GoldenRatio {
+public:
+	GoldenRatio (std::function<double(double)> theFunc, Range theRange, double theEps) :
+		myFunction (theFunc), myRange (theRange), myEps (theEps) {};
+	double Run();
+private:
+	std::function<double(double)> myFunction;
+	Range myRange;
+	double myEps;
+
+};
