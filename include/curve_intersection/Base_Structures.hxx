9169007c6f0e2ea673fc06ebe7ca9fe42f588772
KseniaBastrakova <bastrakova.kseinia@gmail.com>
2018-02-19 18:18:22 +0300

    Add vector to Base_Structures.hxx Add operator == to Point structure

--- include/curve_intersection/Base_Structures.hxx
+++ include/curve_intersection/Base_Structures.hxx
@@ -2,9 +2,9 @@
 #include <cmath>
 
 struct Range {
-	Range(){}
-	Range (double theBegin, double theEnd):
-		Begin (theBegin), End (theEnd) {}
+	Range() {}
+	Range(double theBegin, double theEnd) :
+		Begin(theBegin), End(theEnd) {}
 	double Begin;
 	double End;
 	double Lenght() const {
@@ -13,18 +13,50 @@ struct Range {
 };
 
 struct Point {
-	Point(){}
-	Point (double theX, double theY) :
-		X (theX), Y (theY) {}
-	  Point (const Point& thePoint) {
-		  X = thePoint.X;
-		  Y = thePoint.Y; 
-	  }
+	Point() {}
+	Point(double theX, double theY) :
+		X(theX), Y(theY) {}
+	Point(const Point& thePoint) {
+		X = thePoint.X;
+		Y = thePoint.Y;
+	}
 	double X;
 	double Y;
 };
 
-inline double Distance (Point theFirst, Point theSecond) {
+struct Vector {
+	Vector() {}
+	Vector(double theX, double theY) :
+		X(theX), Y(theY) {}
+	Vector(const Vector& theVector) {
+		X = theVector.X;
+		Y = theVector.Y;
+	}
+	double X;
+	double Y;
+};
+
+inline double VectorMult(Vector vectorFirst, Vector vectorSecound)
+{
+	return vectorSecound.X * vectorFirst.Y - vectorSecound.Y * vectorFirst.X;
+}
+
+inline Point Rotate(Point point, double alpha) {
+	double cosAlpha = cos(alpha);
+	double sinAlpha = sin(alpha);
+	double oldx = point.X;
+	double oldy = point.Y;
+	return Point(oldx * cosAlpha - oldy * sinAlpha, oldx * sinAlpha + oldy * cosAlpha);
+}
+
+
+inline bool IsCollinear(Vector vectorFirst, Vector vectorSecound)
+{
+	return (fabs(VectorMult(vectorFirst, vectorSecound)) < 1e-7);
+}
+
+
+inline double Distance(Point theFirst, Point theSecond) {
 	return (theFirst.X - theSecond.X) + (theFirst.Y - theSecond.Y);
 }
 
@@ -32,8 +64,13 @@ inline bool operator == (Point theFirst, Point theSecond) {
 	return (theFirst.X == theSecond.X && theFirst.Y == theSecond.Y);
 }
 
+inline bool operator == (Range theFirst, Range theSecond) {
+	return (theFirst.Begin == theSecond.Begin && theFirst.End == theSecond.End);
+}
+
+
 inline Point operator - (Point thePoint) {
-	return Point (-thePoint.X, -thePoint.Y);
+	return Point(-thePoint.X, -thePoint.Y);
 }
 
 inline Point operator * (Point thePoint, double aParam) {
@@ -41,20 +78,20 @@ inline Point operator * (Point thePoint, double aParam) {
 }
 
 inline Point operator - (Point thePointLeft, Point thePointRight) {
-	return Point (thePointLeft.X - thePointRight.X, thePointLeft.Y - thePointRight.Y);
+	return Point(thePointLeft.X - thePointRight.X, thePointLeft.Y - thePointRight.Y);
 }
 
-inline double NormSq (Point thePoint) {
+inline double NormSq(Point thePoint) {
 	return (thePoint.X *thePoint.X + thePoint.Y * thePoint.Y);
 }
 
 struct Box {
 	Range myRangeX;
 	Range myRangeY;
-	Box (Range theRangeX, Range theRangeY) :
-		myRangeX (theRangeX), myRangeY (theRangeY) {}
+	Box(Range theRangeX, Range theRangeY) :
+		myRangeX(theRangeX), myRangeY(theRangeY) {}
 	Point Center() {
-		return Point (myRangeX.Begin + (myRangeX.Lenght()) / 2.0, myRangeY.Begin + myRangeY.Lenght() / 2.0);
+		return Point(myRangeX.Begin + (myRangeX.Lenght()) / 2.0, myRangeY.Begin + myRangeY.Lenght() / 2.0);
 	}
 	bool IsMaxX() {
 		return ((myRangeX.Lenght()) > (myRangeY.Lenght()));
