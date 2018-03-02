30e84700c9de36f2aa59c7015466d5a51ccc7504
bastrakova.kseinia <bastrakova.kseinia@gmail.com>
2018-03-02 17:23:08 +0100

    Move Box structure to file

---
+++ include/curve_intersection/Intersection/Box.hxx
@@ -0,0 +1,21 @@
+#pragma once
+#include "Point.hxx"
+#include "Range.hxx"
+
+namespace CurveIntersection {
+	
+struct Box {
+	Range myRangeX;
+	Range myRangeY;
+	Box(Range theRangeX, Range theRangeY) :
+		myRangeX(theRangeX), myRangeY(theRangeY) {}
+	Point Center() {
+		return Point(myRangeX.Begin + (myRangeX.Lenght()) / 2.0, myRangeY.Begin + myRangeY.Lenght() / 2.0);
+	}
+	bool IsMaxX() {
+		return ((myRangeX.Lenght()) > (myRangeY.Lenght()));
+	}
+	double RCharacteristic;
+};
+
+}
